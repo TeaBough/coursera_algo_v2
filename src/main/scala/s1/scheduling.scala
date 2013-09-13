4bf16c10dfbffdda03830e46d62b30d649b940ee
Thibault <thibault.vigouroux@gmail.com>
2013-09-13 20:27:00 +0200

    [S01E01] First version on the main algo

--- src/main/scala/s1/scheduling.scala
+++ src/main/scala/s1/scheduling.scala
@@ -18,34 +18,22 @@ object scheduling {
     def compare(a: Job, b: Job) = {
       val difA = a.weight - a.length
       val difB = b.weight - b.length
-      if (difA == difB){
-        if (a.weight.compare(b.weight) == -1) println(b + " is bigger than " + a)
-        else
-          println(a + " is bigger than " + b)
+      if (difA == difB)
         a.weight.compare(b.weight)
-      }
       else
-      {
-        if (difA.compare(difB) == -1) println(b + " is bigger than " + a)
-        else
-          println(a + " is bigger than " + b)
         difA.compare(difB)
-      }
     }
   }
 
 
   def main(args: Array[String]) {
 
-    println("ref = " + 1.compare(2))
     val lines = scala.io.Source.fromFile("src/main/scala/s1/jobs.txt", "utf-8").getLines.mkString
     val pq = new mutable.PriorityQueue[Job]()(MyOrdering) ++ (List(Job(30,90),Job(10,30),Job(20,40),Job(30,50),Job(20,60),Job(10,50)))
+    val ll = pq.dequeueAll.toList
+    val list_completionTime_weight = ll.foldLeft(List((0,0)))((acc,b) => acc.::((acc.head._1 + b.length,b.weight)))
+    val weighted_completionTime = list_completionTime_weight.foldLeft(0)((acc, b) => acc+b._1*b._2)
+    println(weighted_completionTime)
 
-    println(pq.dequeue())
-    println(pq.dequeue())
-    println(pq.dequeue())
-    println(pq.dequeue())
-    println(pq.dequeue())
-    println(pq.dequeue())
   }
 }
