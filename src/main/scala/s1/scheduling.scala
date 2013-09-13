e6e7f6be2a611a13d6e86c2726a1b9f16b5cb34d
Thibault Vigouroux <thv@octo.com>
2013-09-13 17:58:05 +0200

    Add structure for the first week

---
+++ src/main/scala/s1/scheduling.scala
@@ -0,0 +1,12 @@
+package s1
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: thibault
+ * Date: 13/09/13
+ * Time: 17:56
+ * To change this template use File | Settings | File Templates.
+ */
+object scheduling {
+  def main(args: Array[String]) = println("Hi!")
+}
