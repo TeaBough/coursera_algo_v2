954584e6661b026e3be5497eb5eb7b89a784d63e
Thibault <thibault.vigouroux@gmail.com>
2013-10-11 23:48:09 +0200

    [S02E01] Too slow first version

--- src/main/scala/s5/package.scala
+++ src/main/scala/s5/package.scala
@@ -22,7 +22,7 @@ object s5 {
   }
 
   def main(args: Array[String]) {
-    val input = scala.io.Source.fromFile("src/main/scala/s5/tsp2.txt", "utf-8")
+    val input = scala.io.Source.fromFile("src/main/scala/s5/tsp.txt", "utf-8")
     val splited_input = input.getLines.mkString("\n").split("\n")
     val n = splited_input.head.toInt
     val my_input: List[String] = splited_input.toList.drop(1)
@@ -31,33 +31,34 @@ object s5 {
       val b = x.split(" ")(1).toDouble
       new City(a, b)
     }).toArray
-
-    println(cities.size)
+    val max_size = (1 to 25).toSet.subsets(12).size)
     var map = HashMap[(List[Int], Int), Double]()
     var c_arr = Array.ofDim[Double](n + 1, n + 1)
     map.update((List(1), 1), 0.0)
-    for (s <- 2 to n) {
+    for (s <- 1 to n-1) {
+      println(s)
       val subsets: List[List[Int]] = (2 to n).toSet.subsets(s).map(_.toList.::(1)).toList
-      println(subsets.size)
       for (sub <- subsets) {
-        map.update((sub.sorted, 1), 999999999.999)
-        for (j <- sub) {
-          map.update((sub.sorted, j),
-            sub.foldLeft(77777777.7777)((acc, i) => {
-              if (i != j) {
-                val c: Double = map.get((sub.filter(_ != j), i)).getOrElse(0)
-                val d = distance(cities(i - 1), cities(j - 1))
-                println(d)
-                val res = c + d
-                if (res < acc) res else acc
-              }
-              else acc
-            }))
+        val sorted_sub = sub.sorted
+        map.update((sorted_sub, 1), 999999999.999)
+        for (j <- sorted_sub.filter(_ != 1)) {
+          val min = sorted_sub.foldLeft(77777777.7777)((acc, i) => {
+            if (i != j) {
+              val c: Double = map.get((sorted_sub.filter(_ != j), i)).getOrElse(66666666.666)
+              val d = distance(cities(i - 1), cities(j - 1))
+              val res = c + d
+              if (res < acc) res else acc
+            }
+            else acc
+          })
+          map.update((sorted_sub, j),min)
         }
       }
     }
-    println(map)
-    (2 to n).foreach(x => println(map.get(((1 to n).toList, x)).getOrElse(888.88) + distance(cities(x - 1), cities(0))))
+
+    val res = (2 to n).map(x => map.get(((1 to n).toList, x)).getOrElse(888.88) + distance(cities(x - 1), cities(0)))
+    println(res.foldLeft(9999999999.9999)((acc,num) => math.min(acc,num)))
+
 
 
   }
