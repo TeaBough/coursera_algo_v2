9826912cbb2fc4e98ef4b7100e5c123e7d8a7213
Thibault <thibault.vigouroux@gmail.com>
2013-10-12 06:19:01 +0200

    S05 Use old map and new map instead of keeping everything

--- src/main/scala/s5/package.scala
+++ src/main/scala/s5/package.scala
@@ -31,32 +31,36 @@ object s5 {
       val b = x.split(" ")(1).toDouble
       new City(a, b)
     }).toArray
-    val max_size = (1 to 25).toSet.subsets(12).size)
-    var map = HashMap[(List[Int], Int), Double]()
-    var c_arr = Array.ofDim[Double](n + 1, n + 1)
-    map.update((List(1), 1), 0.0)
+    //val max_size = (1 to 25).toSet.subsets(12).size
+    var new_map = HashMap[(List[Int], Int), Double]()
+    var old_map = HashMap[(List[Int], Int), Double]()
+    //var c_arr = Array.ofDim[Double](max_size,n)
+    old_map.update((List(1), 1), 0.0)
     for (s <- 1 to n-1) {
       println(s)
       val subsets: List[List[Int]] = (2 to n).toSet.subsets(s).map(_.toList.::(1)).toList
+      old_map = new_map
+      old_map.update((List(1), 1), 0.0)
+      new_map = HashMap[(List[Int], Int), Double]()
       for (sub <- subsets) {
         val sorted_sub = sub.sorted
-        map.update((sorted_sub, 1), 999999999.999)
+        old_map.update((sorted_sub, 1), 999999999.999)
         for (j <- sorted_sub.filter(_ != 1)) {
           val min = sorted_sub.foldLeft(77777777.7777)((acc, i) => {
             if (i != j) {
-              val c: Double = map.get((sorted_sub.filter(_ != j), i)).getOrElse(66666666.666)
+              val c: Double = old_map.get((sorted_sub.filter(_ != j), i)).getOrElse(66666666.666)
               val d = distance(cities(i - 1), cities(j - 1))
               val res = c + d
               if (res < acc) res else acc
             }
             else acc
           })
-          map.update((sorted_sub, j),min)
+          new_map.update((sorted_sub, j),min)
         }
       }
     }
 
-    val res = (2 to n).map(x => map.get(((1 to n).toList, x)).getOrElse(888.88) + distance(cities(x - 1), cities(0)))
+    val res = (2 to n).map(x => new_map.get(((1 to n).toList, x)).getOrElse(888.88) + distance(cities(x - 1), cities(0)))
     println(res.foldLeft(9999999999.9999)((acc,num) => math.min(acc,num)))
 
 
